ef03b3d7272ba9f6ca1840fd631ca46c70963f83
jigovind556 <12113078@nitkkr.ac.in>
2023-01-18 21:38:47 +0530

    adding search feature

--- Backend/index.js
+++ Backend/index.js
@@ -64,135 +64,196 @@ app.post("/createuser", (req, res) => {
 
 // CHAT CODE
 
-const path = require('path');
+const path = require("path");
 // const express = require('express');
 // const app = express();
-const http = require('http');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {userJoin, getCurrentUser , userLeave, getRoomUsers} = require('./utils/users');
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 // const cors = require('cors');
 // const dotenv = require('dotenv').config();
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient("mongodb+srv://aeromodelling-signup:Shivam114@cluster0.skf6mst.mongodb.net/auto-mate?retryWrites=true&w=majority");
+const client = new MongoClient(
+  "mongodb+srv://aeromodelling-signup:Shivam114@cluster0.skf6mst.mongodb.net/auto-mate?retryWrites=true&w=majority"
+);
 
-const botName = 'AutoMate ChatBot'
+const botName = "AutoMate ChatBot";
 // app.use(cors());
 
 var collection;
 
-
-
-
 const server = http.createServer(app);
-const io = socketio(server);
+// const io = socketio(server);
+var io = require("socket.io")(server, {
+  cors: {
+    origin: ["http://127.0.0.1:5501", "http://127.0.0.1:3001"],
+  },
+});
 
 // const PORT = 3000 || process.env.port;
 
-
 //Set static folder
-app.use(express.static(path.join(__dirname, '../chat') , async (req, res) => {
-    try{
-        let result = await collection.findOne({"room_name": req.query.room_name});
-        res.send(result)
+app.use(
+  "/chat",
+  express.static(path.join(__dirname, "../chat"), async (req, res) => {
+    try {
+      let result = await collection.findOne({ room_name: req.query.room_name });
+      res.send(result);
+    } catch (e) {
+      res.status(500).send({ message: e.message });
     }
-    catch (e) {
-        res.status(500).send({ message: e.message });
-    }
-}));
+  })
+);
 
 //Run when client connect
-io.on('connection', socket =>{
+io.on("connection", (socket) => {
+  //
+  socket.on("joinRoom", async ({ username, room }) => {
+    try {
+      let result = await collection.findOne({ room_name: room });
 
-    //
-    socket.on('joinRoom', async ({username, room}) =>{
+      if (!result) {
+        await collection.insertOne({ room_name: room, messages: [] });
+      }
 
-        try{
-            let result = await collection.findOne({"room_name" : room});
+      const user = userJoin(socket.id, username, room);
 
-            if(!result){
-                await collection.insertOne({"room_name" : room , messages : []});
-            }
+      socket.join(user.room);
 
-            const user = userJoin(socket.id, username, room);
+      //Welcome connect user
+      socket.emit(
+        "message",
+        formatMessage(botName, "Welcome to AutoMate-Chat")
+      );
 
-            socket.join(user.room);
+      //Broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(botName, ` ${user.username} has joined the chat`)
+        );
 
-            //Welcome connect user
-            socket.emit('message', formatMessage(botName, 'Welcome to AutoMate-Chat'));
+      socket.activeRoom = room;
 
-            //Broadcast when a user connects
-            socket.broadcast
-            .to(user.room)
-            .emit(
-                'message' , 
-                formatMessage(botName, ` ${user.username} has joined the chat`)
-            );
+      //Send users and room info
+      io.to(user.room).emit("roomUser", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
 
-            socket.activeRoom = room;
-
-            //Send users and room info 
-            io.to(user.room).emit('roomUser', {
-                room: user.room,
-                users: getRoomUsers(user.room)
-            })
-
-            // Runs when clients dissconnects
-    socket.on('disconnect', ()=>{
+      // Runs when clients dissconnects
+      socket.on("disconnect", () => {
         const user = userLeave(socket.id);
 
-        if(user){
-            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+        if (user) {
+          io.to(user.room).emit(
+            "message",
+            formatMessage(botName, `${user.username} has left the chat`)
+          );
 
-            //Send users and room info 
-        io.to(user.room).emit('roomUser', {
+          //Send users and room info
+          io.to(user.room).emit("roomUser", {
             room: user.room,
-            users: getRoomUsers(user.room)
-        })
+            users: getRoomUsers(user.room),
+          });
         }
-        
-    });
-    
+      });
 
-    //Listen for  chatMessage
-    socket.on('chatMessage', msg =>{
-        collection.updateOne({"room_name": socket.activeRoom} , {
-            "$push" : {
-                "message" : msg
-            }
-        })
+      //Listen for  chatMessage
+      socket.on("chatMessage", (msg) => {
+        collection.updateOne(
+          { room_name: socket.activeRoom },
+          {
+            $push: {
+              message: msg,
+            },
+          }
+        );
 
         const user = getCurrentUser(socket.id);
 
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  console.log(socket.id + " for io2");
+
+  socket.on("check_mate", async ({ To, From, Date, Time }) => {
+    console.log(To, From, Time);
+    db.query(`select name, travel_time from travel t,user u 
+    where t.Email=u.Email and dest=? and jstart=? and 
+          travel_date=? AND travel_time between SUBTIME(?, 003000) and ADDTIME(?, 003000);`,
+              [To, From, Date, Time, Time], (err, result) => {
+            if (err) {
+              console.log(err.sqlMessage);
+              socket.emit("error", err.sqlMessage);
+            } else {
+              console.log(result);
+              socket.emit("receive_message", result);
+            }
+          });
+  });
+
+  socket.on("submit_new_data", async ({ Email, To, Date, From, Time }) => {
+    try {
+
+      db.query(`
+      Insert into travel (Email,dest,jstart,travel_date,travel_time) values
+      (?,?,?,?,?);`,
+          [Email, To, From, Date, Time], (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          socket.emit("error", err.sqlMessage);
+        } else {
+          console.log(result);
+
+          db.query(`select name, travel_time from travel t,user u 
+          where t.Email=u.Email and dest=? and jstart=? and 
+                travel_date=? AND travel_time between SUBTIME(?, 003000) and ADDTIME(?, 003000);`,
+                    [To, From, Date, Time, Time], (err, result) => {
+            if (err) {
+              console.log(err.sqlMessage);
+              socket.emit("error", err.sqlMessage);
+            } else {
+              console.log(result);
+              socket.emit("receive_message", result);
+            }
+          });
+
+          
         }
-        catch(e){
-            console.error(e);
-        }
+      });
 
-        
-
-    })
-
-    
-    
-})
+      
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 
+});
 
+// these codes are for main page
 
+// 
 
 server.listen(3001, async () => {
-
   try {
     await client.connect().then();
     collection = client.db("auto-mate").collection("chats");
     console.log("Listening on port :", server.address().port);
-} catch (e) {
+  } catch (e) {
     console.error(e);
-}
+  }
 });
-
-
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,28 +1,71 @@
 const Server = "http://127.0.0.1:3001";
-const Server2=Server+"/chat";
-var socket = io(Server2);
-  
+const Server2 = Server + "/chat";
+var socket = io(Server);
+var User;
+
+// eventlisteners
+
+function check_mates() {
+  var nowDate = new Date();
+  var date =
+    nowDate.getFullYear() +
+    "/" +
+    (nowDate.getMonth() + 1) +
+    "/" +
+    nowDate.getDate();
+  var data = {
+    To: document.getElementById("To").value,
+    From: document.getElementById("From").value,
+    Date: date,
+    Time: document.getElementById("Time").value,
+  };
+  if (data.To != "" && data.From != "") {
+    socket.emit("check_mate", data);
+  }
+}
+
+socket.on("receive_message", (message) => {
+  console.log(message);
+  var mate = document.getElementById("Matebox");
+  mate.innerHTML="";
+  for (var i = 0; i < message.length; i++) {
+    console.log(message[i].name);
+    mate.innerHTML += `<div class="people1">
+    <div class="info"> 
+        <div class="name"><h4>`+message[i].name+`</h4></div>
+        <div class="time"><h4>`+message[i].travel_time+`</h4></div>
+        <input type="checkbox" > 
+
+        </div>
+    </div>`;
+  }
+});
+socket.on("error", (message) => {
+  console.log(message);
+});
+
 checkCookie();
 
 function checkCookie() {
   var user = accessCookie("user");
   if (user != "") {
-    var button=document.getElementById("signinButton");
-    button.innerHTML= `<button id="logout" onclick="logout()">Logout</button>`;
-  }
-  else{
-    var button=document.getElementById("signinButton");
-    button.innerHTML= `<button id="login" onclick="login()">Login</button>`;
-
+    User = JSON.parse(user);
+    var button = document.getElementById("signinButton");
+    var button2 = document.getElementById("Submit_buttons");
+    button.innerHTML = `<button id="logout" onclick="logout()">Logout</button>`;
+    button2.innerHTML = `<button id="submit" onclick="submitData()">submit</button>`;
+  } else {
+    var button = document.getElementById("signinButton");
+    button.innerHTML = `<button id="login" onclick="login()">Login</button>`;
   }
 }
 function accessCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -32,10 +75,29 @@ function accessCookie(cname) {
   return "";
 }
 
-function logout(){
-    document.cookie = "user=''; expires=Thu, 18 Dec 2013 12:00:00 UTC";
-    window.location.href="index.html";
+function logout() {
+  document.cookie = "user=''; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+  window.location.href = "index.html";
 }
-function login(){
-    window.location.href="signin.html";
+function login() {
+  window.location.href = "signin.html";
+}
+function submitData() {
+  var nowDate = new Date();
+  var date =
+    nowDate.getFullYear() +
+    "/" +
+    (nowDate.getMonth() + 1) +
+    "/" +
+    nowDate.getDate();
+  var data = {
+    Email: User.id,
+    To: document.getElementById("To").value,
+    From: document.getElementById("From").value,
+    Date: date,
+    Time: document.getElementById("Time").value,
+  };
+  if (data.To != "" && data.From != "") {
+    socket.emit("submit_new_data", data);
+  }
 }
