20f22d2ffdade4414c44e7372934895b44bf891a
jigovind556 <12113078@nitkkr.ac.in>
2023-01-23 16:06:09 +0530

    added notification feature

--- chat/js/index.js
+++ chat/js/index.js
@@ -1,17 +1,17 @@
-const chatForm = document.getElementById('chat-form')
-const chatMessage = document.getElementById('chat')
-const userList = document.getElementById('user_list')
+const chatForm = document.getElementById("chat-form");
+const chatMessage = document.getElementById("chat");
+const userList = document.getElementById("user_list");
 
 const Server2 = Server + "/chat";
 var socket = io(Server);
-
+let User;
 checkCookie();
 function checkCookie() {
   var user = accessCookie("user");
   if (user != "") {
-    
+    User = JSON.parse(user);
   } else {
-    window.location.href="../index.html";
+    window.location.href = "../index.html";
   }
 }
 function accessCookie(cname) {
@@ -30,146 +30,146 @@ function accessCookie(cname) {
   return "";
 }
 
-
 //Get User name and from URL
-const {username , room} = Qs.parse(location.search, {
-  ignoreQueryPrefix : true,
-})
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 console.log(username, room);
 
-function time(message){
-    const div = document.createElement('div');
-    div.classList.add('time');
-    div.innerHTML = `${message.time}`;
-    
-    document.getElementById('chat').appendChild(div);
-}
-
-function timeOld(message){
-  const div = document.createElement('div');
-  div.classList.add('time');
+function time(message) {
+  const div = document.createElement("div");
+  div.classList.add("time");
   div.innerHTML = `${message.time}`;
-  
-  document.getElementById('chat').appendChild(div);
+
+  document.getElementById("chat").appendChild(div);
 }
 
+function timeOld(message) {
+  const div = document.createElement("div");
+  div.classList.add("time");
+  div.innerHTML = `${message.time}`;
 
-
+  document.getElementById("chat").appendChild(div);
+}
 
 // const socket = io();
 
 // Join Chat room
-socket.emit('joinRoom' , {username, room})
+socket.emit("joinRoom", { username, room });
 
 // Get room and users
-socket.on('roomUsers', async ({ room, users }) => {
-    outputRoomName(room);
-    outputUsers(users);
+socket.on("roomUsers", async ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
 
-    let result = await fetch("mongodb+srv://aeromodelling-signup:Shivam114@cluster0.skf6mst.mongodb.net/auto-mate?room=" + room)
-    .then(res => res.json());
-    this.chatMessages = result.messages;
-    
-  });
-
-//Message from server
-socket.on('message', (message) =>{
-    console.log(message);
-    time(message)
-    outputMessage(message)
-
-    //Scroll down
-    chatMessage.scrollTop = chatMessage.scrollHeight;
+  let result = await fetch(
+    "mongodb+srv://aeromodelling-signup:Shivam114@cluster0.skf6mst.mongodb.net/auto-mate?room=" +
+      room
+  ).then((res) => res.json());
+  this.chatMessages = result.messages;
 });
 
-socket.on('output-message', (message) =>{
+//Message from server
+socket.on("message", (message) => {
   console.log(message);
-  
+  time(message);
+  outputMessage(message);
+
+  //Scroll down
+  chatMessage.scrollTop = chatMessage.scrollHeight;
+});
+
+socket.on("output-message", (message) => {
+  console.log(message);
+
   // if(message.length){
   //   message.text.message.forEach((message) =>{
-  //     timeOld(message) 
+  //     timeOld(message)
   //     outputMessageOld(message.text.message)
   //   });
   // }
   const messageVar = message.text.message;
-    for(let i = 0; i < messageVar.length; i++)
-    {
-      timeOld(message)
-      var msg_value = message.text.message[i];
-      outputMessageOld(msg_value);
-      console.log("print");
-    }
-
-
+  for (let i = 0; i < messageVar.length; i++) {
+    timeOld(message);
+    var msg_value = message.text.message[i];
+    outputMessageOld(msg_value);
+    console.log("print");
+  }
 
   //Scroll down
   chatMessage.scrollTop = chatMessage.scrollHeight;
 });
 
 //Message submit
-chatForm.addEventListener('submit' , e =>{
-    e.preventDefault();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    //Get Text Message
-    const msg = e.target.elements.msg.value;
-    data={
-      msg:msg,
-      username:username
-    }
+  //Get Text Message
+  const msg = e.target.elements.msg.value;
+  data = {
+    msg: msg,
+    username: username,
+  };
 
-    //EMit messgae to the server
-    socket.emit('chatMessage', data);
+  //EMit messgae to the server
+  socket.emit("chatMessage", data);
 
-    //clear input
-    e.target.elements.msg.value = '';
-    e.target.elements.msg.focus();
-})
+  //clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
 
 //Output message to dom
 
-function outputMessage(message){
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<div class="user_detail">
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<div class="user_detail">
     <p class="user_name">${message.username}</p>
     <p class="chat_time">${message.time}</p> 
   </div>
   ${message.text}`;
 
-    document.getElementById('chat').appendChild(div);
+  document.getElementById("chat").appendChild(div);
+  if (message.username != User.id) {
+    let showNotification = document.visibilityState !== "visible";
+    if (showNotification) {
+      // Notification code
+      noti(message.text);
+    }
+  }
 }
 
 //Add Room Name
-function outputRoomName(room){
-    console.log(room);
-    outputRoomName.innerText = room;
+function outputRoomName(room) {
+  console.log(room);
+  outputRoomName.innerText = room;
 }
 
-function outputMessageOld(message){
+function outputMessageOld(message) {
   console.log(message);
-  const div = document.createElement('div');
-  div.classList.add('message');
+  const div = document.createElement("div");
+  div.classList.add("message");
   div.innerHTML = `<div class="user_detail">
   <p class="user_name">${message.username}</p>
   <p class="chat_time">${message.time}</p> 
 </div>
 ${message.msg}`;
 
-  document.getElementById('chat').appendChild(div);
+  document.getElementById("chat").appendChild(div);
 }
 
 //Add Room Name
-function outputRoomName(room){
+function outputRoomName(room) {
   console.log(room);
   outputRoomName.innerText = room;
 }
 
-
 function outputUsers(users) {
-    const div = document.createElement('div');
-    div.classList.add('contact')
-    div.innerHTML = `<div class="pic danvers"></div>
+  const div = document.createElement("div");
+  div.classList.add("contact");
+  div.innerHTML = `<div class="pic danvers"></div>
     <div class="badge">
       2
     </div>
@@ -178,12 +178,15 @@ function outputUsers(users) {
     </div>
     <div class="message">
       Hey Peter Parker, you got something for me?
-    </div>` 
-    document.getElementById('user_list').appendChild(div);
-  }
-  
+    </div>`;
+  document.getElementById("user_list").appendChild(div);
+}
 
-document.getElementById('room_name').innerHTML = room;
+document.getElementById("room_name").innerHTML = room;
 
-
-
+function noti(txt) {
+  var title = "Chat-opened";
+  icon = "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
+  var body = txt;
+  var notification = new Notification(title, { body, icon });
+}
