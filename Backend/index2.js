3a770ec4c7fde9e5a1f80d1ed14407112207de47
jigovind556 <12113078@nitkkr.ac.in>
2023-01-23 15:08:24 +0530

    Backend

---
+++ Backend/index2.js
@@ -0,0 +1,469 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+const moment = require("moment");
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "automate.cs0gg5mbtn2b.ap-south-1.rds.amazonaws.com",
+  password: "Shivam114",
+  database: "auto_mate",
+  multipleStatements: true,
+});
+
+// to get user info for signup page
+app.post("/getUser",async (req, res) => {
+  try {
+    var email = req.body.email;
+
+    console.log(email);
+    db.query("select * from user where email =?;", [email], (err, result) => {
+      if (err) {
+        console.log(err.sqlMessage);
+        res.status(102).send(new Error(err.sqlMessage));
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    });
+  } catch (error) {
+    console.log("wrong user");
+    res.status(102).send(new Error("data should not be blank"));
+  }
+});
+
+// to get the user info for account page
+app.post("/getcred",async (req, res) => {
+  try {
+    var email = req.body.email;
+
+    console.log(email);
+    db.query(
+      "select name,Mobile_no from user where email =?;",
+      [email],
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          res.status(102).send(new Error(err.sqlMessage));
+        } else {
+          console.log(result);
+          res.send(result);
+        }
+      }
+    );
+  } catch (error) {
+    console.log("wrong user");
+    res.status(102).send(new Error("data should not be blank"));
+  }
+});
+
+//this function is to create account
+app.post("/createuser",async (req, res) => {
+  try {
+    var name = req.body.name;
+    var Email = req.body.Email;
+    var Mobile_no = req.body.Mobile_no;
+    var Password = req.body.Password;
+
+    db.query(
+      "insert into user (email,name,password,Mobile_no) values(?,?,?,?);",
+      [Email, name, Password, Mobile_no],
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          res.status(102).send(new Error(err.sqlMessage));
+        } else {
+          res.send("success");
+        }
+      }
+    );
+  } catch (error) {
+    console.log("wrong user");
+    res.status(102).send(new Error("data should not be blank"));
+  }
+});
+
+// the function to change password
+app.post("/changepwd",async (req, res) => {
+  try {
+    var email = req.body.email;
+    var OldPwd = req.body.OldPwd;
+    var NewPwd = req.body.NewPwd;
+    var pwd;
+
+    console.log(email);
+    db.query(
+      "select Password from user where email =?;",
+      [email],
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          res.status(102).send(new Error(err.sqlMessage));
+        } else {
+          // console.log(result[0].Password);
+          pwd = result[0].Password;
+          if (pwd == OldPwd) {
+            db.query(
+              `update user set Password=? where Email=?;`,
+              [NewPwd, email],
+              (err, result) => {
+                if (err) {
+                  console.log(err.sqlMessage);
+                  res.status(102).send(new Error(err.sqlMessage));
+                } else {
+                  res.send("updated");
+                }
+              }
+            );
+          } else {
+            res.send("nomatch");
+          }
+        }
+      }
+    );
+  } catch (error) {
+    console.log("wrong user");
+    res.status(102).send(new Error("Wrong data format"));
+  }
+});
+
+// The function to update user detail
+app.post("/updtuser", async (req, res) => {
+  try {
+    var email = req.body.email;
+    var name = req.body.name;
+    var Mob = req.body.Mob;
+    var f1= true,f2=true;
+    console.log(f1,f2);
+    if (name != "" && Mob=="") {
+      db.query(
+        `update user set name=? where Email=?;`,[name,email],
+        (err, result) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            res.status(102).send(new Error(err.sqlMessage));
+          } else {
+            console.log(true);
+            res.send("success");
+          }
+        }
+      );
+    }
+    else if(name == "" && Mob!=""){
+      db.query(
+        `update user set Mobile_no=? where Email=?;`,[Mob,email],
+        (err, result) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            res.status(102).send(new Error(err.sqlMessage));
+          } else {
+            console.log(true);
+            res.send("success");
+          }
+        }
+      );
+    }
+    else if(name!= "" && Mob != ""){
+      db.query(
+        `update user set name=? ,Mobile_no=? where Email=?;`,[name,Mob,email],
+        (err, result) => {
+          if (err) {
+            console.log(err.sqlMessage);
+            res.status(102).send(new Error(err.sqlMessage));
+          } else {
+            console.log(true);
+            res.send("success");
+          }
+        }
+      );
+    }
+   
+  } catch (error) {
+    res.status(102).send(new Error(error));
+  }
+});
+
+// The function to get all places in a city
+app.post("/places", async (req, res) => {
+  try {
+    db.query(
+      "select place from places where city='kurukshetra';",
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          res.status(102).send(new Error(err.sqlMessage));
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  } catch (error) {
+    res.status(102).send(new Error(error));
+  }
+});
+
+// CHAT CODE
+
+const path = require("path");
+// const express = require('express');
+// const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+// const cors = require('cors');
+// const dotenv = require('dotenv').config();
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(
+  "mongodb+srv://aeromodelling-signup:Shivam114@cluster0.skf6mst.mongodb.net/auto-mate?retryWrites=true&w=majority"
+);
+
+const botName = "AutoMate ChatBot";
+// app.use(cors());
+
+var collection;
+
+const server = http.createServer(app);
+// const io = socketio(server);
+var io = require("socket.io")(server, {
+  cors: {
+    origin: [
+      "http://127.0.0.1:5501",
+      "https://jigovind556.github.io",
+      "http://127.0.0.1:3001",
+    ],
+  },
+});
+// const io = require("socket.io")(server, {
+//   allowRequest: (req, callback) => {
+//     const noOriginHeader = req.headers.origin === undefined;
+//     callback(null, noOriginHeader);
+//   }
+// });
+// const PORT = 3000 || process.env.port;
+
+//Set static folder
+app.use(
+  "/chat",
+  express.static(path.join(__dirname, "../chat"), async (req, res) => {
+    try {
+      let result = await collection.findOne({ room_name: req.query.room_name });
+      res.send(result);
+    } catch (e) {
+      res.status(500).send({ message: e.message });
+    }
+  })
+);
+
+//Run when client connect
+io.on("connection", (socket) => {
+  //
+  socket.on("joinRoom", async ({ username, room }) => {
+    try {
+      let result = await collection.findOne({ room_name: room });
+
+      if (!result) {
+        await collection.insertOne({
+          room_name: room,
+          messages: [],
+          username: username,
+        });
+      }
+
+      const user = userJoin(socket.id, username, room);
+
+      socket.join(user.room);
+
+      //Finding previous history
+      collection.findOne({ room_name: room }).then((msg) => {
+        socket.emit("output-message", formatMessage(user.username, msg));
+      });
+
+      //Welcome connect user
+      socket.emit(
+        "message",
+        formatMessage(botName, "Welcome to AutoMate-Chat")
+      );
+
+      //Broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(botName, ` ${user.username} has joined the chat`)
+        );
+
+      socket.activeRoom = room;
+
+      //Send users and room info
+      io.to(user.room).emit("roomUser", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+
+      // Runs when clients dissconnects
+      socket.on("disconnect", () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+          io.to(user.room).emit(
+            "message",
+            formatMessage(botName, `${user.username} has left the chat`)
+          );
+
+          //Send users and room info
+          io.to(user.room).emit("roomUser", {
+            room: user.room,
+            users: getRoomUsers(user.room),
+          });
+        }
+      });
+
+      //Listen for  chatMessage
+      socket.on("chatMessage", ({ msg, username }) => {
+        const time = moment().format("h:mm a");
+        collection.updateOne(
+          { room_name: socket.activeRoom },
+          {
+            $push: {
+              message: { username, msg, time },
+            },
+          }
+        );
+
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  console.log(socket.id + " for io2");
+
+  socket.on("check_mate", async ({ To, From, Date, Time }) => {
+    console.log(To, From, Time);
+    db.query(
+      `select room_name , travel_time ,chatRoom_id ,No_of_person from chatroom 
+      where  dest=? and jstart=? and
+      date=? AND travel_time between SUBTIME(?, 003000) and ADDTIME(?, 003000);`,
+      [To, From, Date, Time, Time],
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          socket.emit("error", err.sqlMessage);
+        } else {
+          console.log(result);
+          socket.emit("receive_message", result);
+        }
+      }
+    );
+  });
+
+  socket.on(
+    "submit_new_data",
+    async ({ Email, To, Date, From, Time, chatid }) => {
+      try {
+        console.log(chatid);
+        // var chatid="1245624";
+        db.query(
+          `
+      
+      insert into chatroom (chatRoom_id,dest,jstart,room_name,travel_time,date,NO_of_person) values (?,?,?,
+        (select name from user where Email=?),
+      ?,?,"1");
+      insert into chatgroup (Email, chatRoom_id) values (?,?); `,
+          [chatid, To, From, Email, Time, Date, Email, chatid],
+          (err, result) => {
+            if (err) {
+              console.log(err.sqlMessage);
+              socket.emit("error", err.sqlMessage);
+            } else {
+              console.log(result);
+
+              db.query(
+                `select room_name , travel_time ,chatRoom_id ,No_of_person from chatroom 
+          where  dest=? and jstart=? and
+          date=? AND travel_time between SUBTIME(?, 003000) and ADDTIME(?, 003000);`,
+                [To, From, Date, Time, Time],
+                (err, result) => {
+                  if (err) {
+                    console.log(err.sqlMessage);
+                    socket.emit("error", err.sqlMessage);
+                  } else {
+                    console.log(result);
+                    socket.emit("receive_message", result);
+                    socket.emit("createChat", chatid);
+                  }
+                }
+              );
+            }
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+  socket.on("joinchat", async ({ Email, To, Date, From, Time, chatid }) => {
+    console.log("Email " + Email + " chatid " + chatid);
+    db.query(
+      `select * from chatgroup where Email=? and chatRoom_id=? ;`,
+      [Email, chatid],
+      (err, result) => {
+        if (err) {
+          console.log(err.sqlMessage);
+          socket.emit("error", err.sqlMessage);
+        } else {
+          if (result[0]) {
+            socket.emit("createChat", chatid);
+          } else {
+            db.query(
+              `
+              update chatroom set No_of_person = No_of_person+1 where chatroom_id=?;
+              insert into chatgroup (Email, chatRoom_id) values (?,?);`,
+              [chatid, Email, chatid],
+              (err, result) => {
+                if (err) {
+                  console.log(err.sqlMessage);
+                  socket.emit("error", err.sqlMessage);
+                } else {
+                  console.log(result);
+                  socket.emit("createChat", chatid);
+                }
+              }
+            );
+          }
+        }
+      }
+    );
+  });
+});
+
+// these codes are for main page
+
+//
+
+server.listen(3001, async () => {
+  try {
+    await client.connect().then();
+    collection = client.db("auto-mate").collection("chats");
+    console.log("Listening on port :", server.address().port);
+  } catch (e) {
+    console.error(e);
+  }
+});
