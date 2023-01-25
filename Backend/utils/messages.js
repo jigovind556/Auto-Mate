05d2e9e0ad3e89f73930b1c9caa8d11c9df33fa8
Shivam Varshney <12111043@nitkkr.ac.in>
2023-01-25 23:20:25 +0530

    timezone correct

--- Backend/utils/messages.js
+++ Backend/utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text){
     return{
         username,
         text,
-        time : moment().format("h:mm a")
+        time : moment().utcOffset("+05:30").format("h:mm a")
     }
 }
 
