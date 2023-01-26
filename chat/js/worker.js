1c9a1b5f9e5aea59f595b55ffff5d5eda6d2b5a8
Shivam Varshney <12111043@nitkkr.ac.in>
2023-01-27 04:50:13 +0530

    push notification added

---
+++ chat/js/worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("push", e => {
+    const data = e.data.json();
+    self.registration.showNotification(
+        data.title, // title of the notification
+        {
+            body: "Push notification from section.io", //the body of the push notification
+            image: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
+            icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/" // icon 
+        }
+    );
+});
